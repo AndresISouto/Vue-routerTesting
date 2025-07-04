06157336e7b0bc8f3fd29c6adca498f38ff646b0
AndrseISouto <186386252+AndresISouto@users.noreply.github.com>
2025-07-04 20:19:22 +0200

    fixed nav bar

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -1,12 +1,21 @@
 <template>
   <nav id="nav">
-    <router-link to="/">Home </router-link>
-    <router-link to="brazil">Brazil </router-link>
-    <router-link to="hawaii">Hawaii </router-link>
-    <router-link to="jamaica">Jamaica </router-link>
-    <router-link to="panama">Panama </router-link>
+    <router-link to="/">Home</router-link>
+    <router-link v-for="destination in destinations" :key="destination.id" :to="{
+      name: 'destination.show', params: {
+        id: destination.id
+      }
+    }">
+      {{ destination.name }}
+    </router-link>
   </nav>
 </template>
+<script setup>
+import data from "@/data.json"
+import { ref } from "vue";
+
+const destinations = ref(data.destinations)
+</script>
 <style scoped>
 #nav .activeLink {
   color: lightblue;
