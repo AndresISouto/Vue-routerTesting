d7338bcbb3b1ef4e2413d86db578b22af0acb27a
AndrseISouto <186386252+AndresISouto@users.noreply.github.com>
2025-07-04 13:28:19 +0200

    index for routers

--- src/main.js
+++ src/main.js
@@ -1,17 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import { createRouter, createWebHistory } from "vue-router"
-import Home from "@/views/Home.vue"
-import About from "@/views/About.vue"
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    { path: '/', name: "Home", component: Home },
-    { path: '/about', name: "About", component: About }
-  ]
-})
-
+import { router } from "@/router/index.js"
 createApp(App)
   .use(router)
   .mount('#app')
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,4 @@
 <template>
-
   <div class="about">
     <h1>about</h1>
   </div>
