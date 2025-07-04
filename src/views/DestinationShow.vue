63e99cdb5c731569734639b63aa1504ef5934c38
AndrseISouto <186386252+AndresISouto@users.noreply.github.com>
2025-07-04 19:51:41 +0200

    parametrized routing

---
+++ src/views/DestinationShow.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <section class="destination">
+      <h1>{{ destination.name }}</h1>
+      <div class="destination-details">
+        <img :src="`/images/${destination.image}`" alt="">
+        <p>{{ destination.description }}</p>
+      </div>
+    </section>
+  </div>
+</template>
+<script setup>
+import { useRoute } from 'vue-router'
+import { computed } from 'vue';
+import sourceData from "@/data.json"
+
+const route = useRoute()
+const destination = computed(() => {
+  const id = parseInt(route.params.id)
+  return sourceData.destinations.find(e => e.id === id)
+})
+</script>
+<style scoped></style>
