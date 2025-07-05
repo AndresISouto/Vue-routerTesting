df6c68c16e3eb6df1d9d3f7975dabfb326f5d809
AndrseISouto <186386252+AndresISouto@users.noreply.github.com>
2025-07-05 13:51:02 +0200

    add experiences

---
+++ src/components/ExperienceCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="card">
+    <img :src="`/images/${experience.image}`" alt="no encontrado">
+    <span class="card__text">
+      {{ experience.name }}
+    </span>
+  </div>
+</template>
+<script setup>
+const props = defineProps({
+  experience: {
+    type: Object,
+    required: true
+  }
+})
+console.log(props.experience.slug)
+</script>
+<style scoped></style>
--- src/views/DestinationShow.vue
+++ src/views/DestinationShow.vue
@@ -7,38 +7,52 @@
         <p>{{ destination.description }}</p>
       </div>
     </section>
+    <section class="experiences">
+      <h2>Top experiences in {{ destination.name }}</h2>
+      <div class="cards">
+        <ExperienceCard v-for="experience in destination.experiences" :key="experience.slug" :experience="experience">
+        </ExperienceCard>
+      </div>
+    </section>
   </div>
 </template>
 <script setup>
 import { useRoute } from 'vue-router'
 import { watch, computed, onMounted, ref } from 'vue';
 import sourceData from "@/data.json"
+import ExperienceCard from '@/components/ExperienceCard.vue';
 
-const destination = ref(null)
-const route = useRoute()
-// const destination = computed(() => {
-//   const id = parseInt(route.params.id)
-//   return sourceData.destinations.find(e => e.id === id)
-// })
-//esto es para simular una query
-async function fetchDestination() {
-  const response = await fetch(`/api/${route.params.slug}.json`)
-  if (!response.ok) throw new Error("response not ok")
-  destination.value = await response.json()
-}
-
-onMounted(() => {
-  fetchDestination()
-  // esto esta guay si hay problemas de rendimiento o si se tarda en cambiar entre paginas
-  // watch(
-  //   () => route.params.id || route.params.slug,
-  //   () => {
-  //     destination.value = null // Optional: Clear previous data while loading
-  //     fetchDestination()
-  //   },
-  //   // { immediate: false } // Already fetched on mount
-  // )
-
+const props = defineProps({
+  id: {
+    type: Number,
+    required: true
+  }
 })
+// const destination = ref(null)
+const route = useRoute()
+const destination = computed(() => {
+  // const id = parseInt(route.params.id)
+  return sourceData.destinations.find(e => e.id === props.id)
+})
+//esto es para simular una query
+// async function fetchDestination() {
+//   const response = await fetch(`/api/${route.params.slug}.json`)
+//   if (!response.ok) throw new Error("response not ok")
+//   destination.value = await response.json()
+// }
+
+// onMounted(() => {
+//   fetchDestination()
+//   // esto esta guay si hay problemas de rendimiento o si se tarda en cambiar entre paginas, pero se puede usar la key en el route-view
+//   // watch(
+//   //   () => route.params.id || route.params.slug,
+//   //   () => {
+//   //     destination.value = null // Optional: Clear previous data while loading
+//   //     fetchDestination()
+//   //   },
+//   //   // { immediate: false } // Already fetched on mount
+//   // )
+//
+// })
 </script>
 <style scoped></style>
